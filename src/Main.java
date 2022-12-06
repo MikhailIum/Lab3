47d21b91d6d3fa418e36c99e27ccfe7ddd68d93d
MikhailIum <73828917+MikhailIum@users.noreply.github.com>
2022-12-06 11:51:08 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Spacesuit spacesuit = new Spacesuit(new Jumpsuit(), new Helmet(), new Spaceboots());
+        System.out.println(spacesuit.toString());
+
+    }
+}
