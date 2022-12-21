effa2f3377833d6d4bea442b4dfd2e26df6d938f
MikhailIum <73828917+MikhailIum@users.noreply.github.com>
2022-12-21 11:51:46 +0300

    Add files via upload

---
+++ src/enum/Capital.java
@@ -0,0 +1,14 @@
+public enum Capital {
+    LOW{
+        public Capital opposite(){
+            return HIGH;
+        }
+    },
+    HIGH{
+        public Capital opposite(){
+            return LOW;
+        }
+    };
+
+     public abstract Capital opposite();
+}
